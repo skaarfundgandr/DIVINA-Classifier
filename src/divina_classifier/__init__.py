be05425912b5b51864bf0c6096cbb622ca324af5
jm-eospc <mac2x.manos@gmail.com>
2026-03-01 16:40:39 +0800

    FEAT: Add VGG16 model for image classification and update main script

--- src/divina_classifier/__init__.py
+++ src/divina_classifier/__init__.py
@@ -1,5 +1,6 @@
 from divina_classifier.models.resnet import ResNet50Classifier
+from divina_classifier.models.vgg16 import VGG16Classifier
 from divina_classifier.utils.image_processing import load_image, preprocess_image
 from divina_classifier.globals import DEVICE
 
-__all__ = ["ResNet50Classifier", "load_image", "preprocess_image", "DEVICE"]
+__all__ = ["ResNet50Classifier", "load_image", "preprocess_image", "DEVICE", "VGG16Classifier"]
